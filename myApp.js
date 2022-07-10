af91473a0e9b795ea5457fa1ea2bba5a9e87249a
juan rebolledo <juanjoserebolledo18@gmail.com>
2022-07-10 17:42:52 -0400

    ....

--- myApp.js
+++ myApp.js
@@ -39,6 +39,9 @@ app
     .route('/name')
     .get((req, res) => {
         res.json({"name": `${req.query.first} ${req.query.last}`});
+    })
+    .post((req, res) => {
+        res.json({"name": `${req.body.first} ${req.body.last}`});
     });
 
 
